bc22b7922c0e76078926e2ef15a02b3351b32820
nawalhaji <nawalhaji13@gmail.com>
2024-04-26 20:50:27 -0700

    AI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <div className="flex color-black">
+    <main className="flex min-h-screen flex-col items-left justify-between p-24 bg-beige w-100%">
+      <div className="flex color-black w-100%">
       <i className="bi bi-arrow-right-circle-fill color-black icon-105"></i>
-      <div className="grid bg-pink border-2 border-black">
-        <h1>akjsgdfoaiusdg</h1>
+
+      <i className="bi bi-arrow-90deg-right relative top-0 left-0"></i>
+      <div className=" bg-white h-20 w-100 border-2 border-black name rounded-sm">
+        <input className = "object-contain" type="text"/>
       </div>
       </div>
     </main>
