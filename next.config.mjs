18dae63dd7546a8f540688c5a7178540b779d83c
rina-reimer <rinareimer616@gmail.com>
2024-04-27 19:11:05 -0700

    finished courses section

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['pdf2json'],
   },
+  images: {
+    domains: ['i.pinimg.com'],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import './components/embla.css'
 import ElasticCarousel from './components/ElasticCarousel'
 
 export default function Home() {
+  var search = "Ask B0b Anything!"
   
   const OPTIONS: EmblaOptionsType = {}
   const SLIDE_COUNT = 5
@@ -18,23 +19,23 @@ export default function Home() {
       <main className="grid">    
         {/* ask b0b anything */}
         <div className="flex w-full h-fit bg-white border-4 border-black rounded-box box-shadow items-center justify-end">
-          <input type="text" value="Ask B0b Anything" className="m-4 w-11/12"></input>
+          <input type="text" className="mx-4 w-11/12 h-10 rounded-md"></input>
           <Link href="/chatbot"><i className="bi bi-chat-dots icon-105 m-4"></i></Link>
         </div>
 
 {/* recommended jobs */}
         <div className="flex py-8 items-center">
-          <div className="w-48">
-            <Image src="/sprite-left.png" priority={true} width={300} height={300} alt={"Job Bob facing right"} />
+          <div className="w-4/12">
+            <Image src="/sprite-left.png" priority={true} width={400} height={400} alt={"Job Bob facing right"} />
           </div>
           <div className="grid w-full">  
             <div className="flex items-center w-full h-fit bg-seafoam border-4 border-black rounded-box box-shadow my-4">
-              <span className="text-3xl p-8">Here are some recommended jobs for you!</span>
+              <span className="text-xl p-8">Here are some recommended jobs for you!</span>
             </div>
             <EmblaCarousel slides={SLIDES} options={OPTIONS} />
 {/* resume scanner */}
             <div className="flex items-center w-full h-fit bg-orange border-4 border-black rounded-box box-shadow my-4">
-              <Link className="text-3xl p-8" href="/resume-scanner">Click here to scan your resume!</Link>
+              <Link className="text-xl p-8" href="/resume-scanner">Click here to scan your resume!</Link>
             </div>
           </div>
         </div>
